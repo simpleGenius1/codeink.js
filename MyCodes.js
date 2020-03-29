29dc1e632cc5fda7222b1289005a20d2388e0149
simpleGenius1 <62834992+simpleGenius1@users.noreply.github.com>
2020-03-29 13:54:48 +0100

    Update MyCodes.js

--- MyCodes.js
+++ MyCodes.js
@@ -8,3 +8,17 @@ oddNumber.push(i)
 
 document.write(oddNumber)
 alert(oddNumber)
+
+
+// OddNumber Using function 
+
+function myFunction() {
+
+for (var i=1; i<=200; i+=2)
+    {
+    if(i % 2 == 0); document.write(i + ' ') 
+    }
+} // still needs fixing to remove Undefined.
+
+console.log(myFunction()) // calling the function.
+
